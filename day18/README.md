da9801c80c6f6dc73e5593800fcba40f6e4ed8eb
jscott <jscott@roguecu.org>
2021-12-21 07:12:48 -0800

    setup for day 18

---
+++ day18/README.md
@@ -0,0 +1,6 @@
+# Day 18: Snailfish
+
+[Day 18 Puzzle](https://adventofcode.com/2021/day/18)
+
++ Part 1: ``.
++ Part 2: ``.
---
+++ day18/day18.py
@@ -0,0 +1,21 @@
+import argparse
+import os
+
+
+def read_input(filepath: str):
+    with open(filepath, 'r') as f:
+        return f.read()
+
+
+def init_parser() -> str:
+    parser = argparse.ArgumentParser(description="Advent of Code day 17 solution.")
+    parser.add_argument('input', metavar='FILE', type=str, nargs=1, help="Path to input data.")
+    args = parser.parse_args()
+    return os.path.realpath(args.input[0])
+
+
+if __name__ == "__main__":
+    path = init_parser()
+    input_ = read_input(path)
+    print(input_)
+
